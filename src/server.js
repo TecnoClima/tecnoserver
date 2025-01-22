f37e1be7c5b1cd392d8b7d50a71e38c052331f50
Leo Monay <leonardomonay@gmail.com>
2025-01-22 07:17:00 -0300

    update index.js with https server data

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
-const https = require("https"); // Agrega esta línea para utilizar HTTPS
 const fs = require("fs");
 const path = require("path");
 const routes = require("./routes/index.js");
@@ -19,43 +18,6 @@ accessLogStream.on("error", (err) => {
 });
 
 const server = express();
-
-// Rutas de los certificados
-const SSL_KEY_PATH = "/etc/ssl/private/server.key"; // Ruta a tu llave privada
-const SSL_CERT_PATH = "/etc/ssl/certs/server.crt"; // Ruta a tu certificado
-
-// Verifica si los certificados existen
-if (fs.existsSync(SSL_KEY_PATH) && fs.existsSync(SSL_CERT_PATH)) {
-  // Cargar los certificados SSL
-  const options = {
-    key: fs.readFileSync(SSL_KEY_PATH),
-    cert: fs.readFileSync(SSL_CERT_PATH),
-    passphrase: "tu_contraseña", // Contraseña de la llave privada (si corresponde)
-  };
-
-  // Crear el servidor HTTPS
-  https.createServer(options, server).listen(3001, () => {
-    console.log("Servidor HTTPS corriendo en el puerto 3001");
-  });
-
-  // Redirigir tráfico HTTP a HTTPS (opcional, si usas HTTP en un puerto diferente)
-  const http = require("http");
-  http
-    .createServer((req, res) => {
-      res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-      res.end();
-    })
-    .listen(3000, () => {
-      console.log("Redireccionando tráfico HTTP al puerto 3001");
-    });
-} else {
-  console.error("Certificados SSL no encontrados. Iniciando en HTTP...");
-
-  // Si los certificados no existen, inicia el servidor en HTTP en el puerto 3001
-  server.listen(3001, () => {
-    console.log("Servidor HTTP corriendo en el puerto 3001");
-  });
-}
 
 const { CLIENT_URL } = process.env;
 
