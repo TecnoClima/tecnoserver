6f196b0d9adb791d6f80ffce84968531575f9de1
Leo Monay <leonardomonay@gmail.com>
2026-06-06 16:21:38 -0300

    update task deletions

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -12,104 +12,131 @@ async function setTasks(req, res) {
     // get body data
     const { device } = req.body;
     const { program } = req.body;
-    const { year, name, frequency, cost, observations } = program;
-    // build task data
-    const data = { year, name, frequency, observations };
-    const plant = await Plant.findOne({ name: program.plant });
-    const strategies = (await Strategy.find({ year, plant: plant._id })).map(
-      (strategy) => strategy._id,
-    );
-    const newStrategy = (
-      await Strategy.findOne({ year, plant: plant._id, name })
-    )._id;
-    const responsible = req.body.program.responsible
-      ? await User.findOne({ idNumber: req.body.program.responsible.id })
-      : undefined;
-    data.cost = cost || 0;
-    data.plant = plant._id;
-    data.strategy = newStrategy;
-    data.responsible = responsible && responsible._id;
-
-    // create or update data for devices
     const devices = await Device.find({ code: device });
 
-    try {
-      for await (let device of devices) {
-        //search tasks already assigned to device this year
-        const currentTasks = await Task.find({
-          device: device._id,
-          strategy: strategies,
+    if (!program?.name) {
+      for (const deviceDoc of devices) {
+        const tasks = await Task.find({ device: deviceDoc._id });
+
+        await TaskDates.deleteMany({
+          task: { $in: tasks.map((t) => t._id) },
         });
-        //select all dates which are already related to workOrders
-        const dates = await TaskDates.find({
-          task: currentTasks.map((t) => t._id),
-        }).populate("workOrders");
-        //make an array with all non repeated dates
-        const datesKept = [
-          ...new Set(
-            dates
-              .filter((date) => !!date.workOrders[0])
-              .map((date) => date.date),
-          ),
-        ];
-        //search and update or create the task.
-        let assignedTask = currentTasks.find(
-          (task) => task.strategy === newStrategy,
-        );
-        if (!assignedTask) {
-          const task = await Task({ device: device._id, ...data });
-          assignedTask = await task.save();
-        } else {
-          assignedTask = await Task.findByIdAndUpdate(assignedTask._id, data);
-        }
-        //add already related dates to the task
-        for await (let date of datesKept) {
-          let ordersArray = [];
-          for (let taskDate of dates.filter(
-            (item) =>
-              new Date(item.date).toISOString() ===
-              new Date(date).toISOString(),
-          )) {
-            for (let order of taskDate.workOrders) {
-              if (!ordersArray.map((order) => order.code).includes(order.code))
-                ordersArray.push(order);
-            }
-          }
-          const newTaskDate = await TaskDates({
-            task: assignedTask._id,
-            date: date,
-            workOrders: ordersArray.map((order) => order._id),
-          });
-          await newTaskDate.save();
-        }
-        //delete all possible dates from other tasks.
-        await TaskDates.deleteMany({ _id: dates.map((e) => e._id) });
-        //delete all possible tasks which where assigned from other strategies.
+
         await Task.deleteMany({
-          device: device._id,
-          _id: { $ne: assignedTask._id },
+          device: deviceDoc._id,
         });
-        results.created.push(device.code);
       }
-    } catch (e) {
-      console.log(e);
-      results.errors.push({
-        code: device.code,
-        name: device.name,
-        error: e.message,
+
+      return res.status(200).send({
+        removed: devices.map((d) => d.code),
       });
+    } else {
+      const { year, name, frequency, cost, observations } = program;
+
+      // build task data
+      const data = { year, name, frequency, observations };
+      const plant = await Plant.findOne({ name: program.plant });
+      const strategies = (await Strategy.find({ year, plant: plant._id })).map(
+        (strategy) => strategy._id
+      );
+      const newStrategy = (
+        await Strategy.findOne({ year, plant: plant._id, name })
+      )._id;
+      const responsible = req.body.program.responsible
+        ? await User.findOne({ idNumber: req.body.program.responsible.id })
+        : undefined;
+      data.cost = cost || 0;
+      data.plant = plant._id;
+      data.strategy = newStrategy;
+      data.responsible = responsible && responsible._id;
+
+      // create or update data for devices
+      const devices = await Device.find({ code: device });
+
+      for await (let device of devices) {
+        try {
+          //search tasks already assigned to device this year
+          const currentTasks = await Task.find({
+            device: device._id,
+            strategy: strategies,
+          });
+          //select all dates which are already related to workOrders
+          const dates = await TaskDates.find({
+            task: currentTasks.map((t) => t._id),
+          }).populate("workOrders");
+          //make an array with all non repeated dates
+          const datesKept = [
+            ...new Set(
+              dates
+                .filter((date) => !!date.workOrders[0])
+                .map((date) => date.date)
+            ),
+          ];
+          //search and update or create the task.
+          let assignedTask = currentTasks.find(
+            (task) => task.strategy?.toString() === newStrategy.toString()
+          );
+          if (!assignedTask) {
+            const task = await Task({ device: device._id, ...data });
+            assignedTask = await task.save();
+          } else {
+            assignedTask = await Task.findByIdAndUpdate(
+              assignedTask._id,
+              data,
+              { new: true }
+            );
+          }
+          //add already related dates to the task
+          for await (let date of datesKept) {
+            let ordersArray = [];
+            for (let taskDate of dates.filter(
+              (item) =>
+                new Date(item.date).toISOString() ===
+                new Date(date).toISOString()
+            )) {
+              for (let order of taskDate.workOrders) {
+                if (
+                  !ordersArray.map((order) => order.code).includes(order.code)
+                )
+                  ordersArray.push(order);
+              }
+            }
+            const newTaskDate = await TaskDates({
+              task: assignedTask._id,
+              date: date,
+              workOrders: ordersArray.map((order) => order._id),
+            });
+            await newTaskDate.save();
+          }
+          //delete all possible dates from other tasks.
+          await TaskDates.deleteMany({ _id: dates.map((e) => e._id) });
+          //delete all possible tasks which where assigned from other strategies.
+          await Task.deleteMany({
+            device: device._id,
+            _id: { $ne: assignedTask._id },
+          });
+          results.created.push(device.code);
+        } catch (e) {
+          console.log(e);
+          results.errors.push({
+            code: device.code,
+            name: device.name,
+            error: e.message,
+          });
+        }
+      }
+      results.created = {
+        device: results.created,
+        strategy: {
+          ...data,
+          plant: plant.name,
+          responsible: responsible
+            ? { id: responsible.idNumber, name: responsible.name }
+            : undefined,
+        },
+      };
+      res.status(200).send(results);
     }
-    results.created = {
-      device: results.created,
-      strategy: {
-        ...data,
-        plant: plant.name,
-        responsible: responsible
-          ? { id: responsible.idNumber, name: responsible.name }
-          : undefined,
-      },
-    };
-    res.status(200).send(results);
   } catch (e) {
     console.log(e);
     res.status(400).send({ error: e.message });
@@ -123,7 +150,7 @@ async function taskOrders(req, res) {
 
     await TaskDates.updateMany(
       { workOrders: workOrder._id },
-      { $pull: { workOrders: workOrder._id } },
+      { $pull: { workOrders: workOrder._id } }
     );
 
     if (date) {
@@ -227,7 +254,7 @@ async function taskDeviceList(req, res) {
       ) {
         let inReclaimed = reclaimed.find(
           (element) =>
-            JSON.stringify(element._id) === JSON.stringify(device._id),
+            JSON.stringify(element._id) === JSON.stringify(device._id)
         );
         const task = plan.find((task) => task.device.code === device.code);
 
